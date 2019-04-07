305d92d24b6afe9602d7114bf72822c265fec7a2
wat_s_on <wat_s_on@outlook.jp>
2019-04-07 23:29:34 +0900

    新規作成

---
+++ Editorial_summary/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Editorial_summary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "要約を入力してください。";
+
+            return View();
+        }
+    }
+}
